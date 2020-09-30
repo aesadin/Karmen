755170c51c32dc14a470339be1e138533ffafdbe
Allison Sadin <aesadin@gmail.com>
2020-09-30 15:51:53 -0700

    add public folder and install

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,19 +22,23 @@ import '../App.css';
 
 export default App;
 
-class App extends Component {
+class App extends Component { // decide if you want NON-signed in user to be able to head to dashboard which might be like fund list except with no functionality? just displays random fundraisers
   render() {
     return (
       <Router>
         <Switch>
           <Route path="/signin" exact component={Signin} />
           {/* <Route path="/signout" exact component={Signout} /> */}
-          <Route path="/dashboard" component={withAuthentication(Dashboard)} />
-          <Route path="/" component={AppControl} />
+          <Route path="/" component={withAuthentication(Dashboard)} />
+          <Route path="/appControl" component={AppControl} />
         </Switch>
       </Router>
     );
   }
 }
+
+// the AppControl component should also probably be wrapped in the withAuthentication because that is where all the functionality happens
+// the landing board can be the dashboard where a swipe functionality shows diffrent fundraisers and allows users to search based on location and tags
+// but only logged in users can add a fundraiser to the list and verify other fundraisers
 
 export default App;
