7f2aa6295a9763058c471f55fc01ea1142ebf8c9
Allison Sadin <aesadin@gmail.com>
2020-10-02 11:39:38 -0700

    added firebase json files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from "./Header";
+import FundControl from "./FundControl";
+
+
+import '../App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <FundControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
